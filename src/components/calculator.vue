dea3c84aba6cab36215f4cfbe97f534b68d5f4ed
LiuTao <1141613534@qq.com>
2023-10-10 22:01:55 +0800

    完成计划部分

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="body">
-    <div>
-      search
-      <el-select v-model="value" filterable placeholder="请选择">
-        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
-    <div>
-      <span>g</span><el-input class="m_input" ></el-input>
-    </div>
-    <div>
-      <span>d</span><el-input class="s_input" ></el-input>
-      <span>a</span><el-input class="s_input" ></el-input>
-      <span>e</span><el-input class="s_input" ></el-input>
-    </div>
-    <div>
-    radio
-      <el-radio v-model="radio" label="1">备选项</el-radio>
-      <el-radio v-model="radio" label="2">备选项</el-radio>
-    </div>
-    <div>
-    check
-    <el-checkbox v-model="checked">备选项</el-checkbox>
-    <el-checkbox v-model="checked">备选项</el-checkbox>
-    <el-checkbox v-model="checked">备选项</el-checkbox>
-    </div>
-    <div>
-      <el-button>add</el-button>
-    </div>
-    <div>
+
+    <el-form ref="form" :model="form" label-width="auto" size="mini">
+
+      <el-form-item label="搜索">
+        <el-select v-model="value" filterable placeholder="请选择">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="输入">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="选项">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="选项1" name="type"></el-checkbox>
+          <el-checkbox label="选项2" name="type"></el-checkbox>
+          <el-checkbox label="选项3" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="计算方式">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="质量"></el-radio>
+          <el-radio label="营养素"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">计算</el-button>
+        <el-button type="primary" @click="onSubmit">添加</el-button>
+      </el-form-item>
+    </el-form>
+    <div class="view_box">
       <el-progress type="circle" :percentage="25" :width="50"></el-progress>
       <el-progress type="circle" :percentage="25" :width="50"></el-progress>
       <el-progress type="circle" :percentage="25" :width="50"></el-progress>
@@ -47,25 +48,37 @@ export default {
     return {
       radio: '',
       options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }],
-        value: '',
-        checked: false
+        value: '选项1',
+        label: '黄金糕'
+      }],
+      value: '',
+      checked: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
 .body {
-  background-color: #f0f0f0;
-  -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); 
-  box-shadow: 0px 10px 13px -7px #a1a1a1, 5px 5px 15px 5px rgba(0,0,0,0);
+  -webkit-box-shadow: 10px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+  box-shadow: 10px 10px 13px -7px #a1a1a1, 5px 5px 15px 5px rgba(0, 0, 0, 0);
   border-radius: 10px;
   padding: 3%;
   margin-bottom: 10px;
+}
+
+.good_input {
+  height: 10px;
 }
 
 .b_red {
@@ -78,5 +91,10 @@ export default {
 
 .s_input {
   width: 80px;
+}
+
+.view_box{
+  display: flex;
+  justify-content: space-around;
 }
 </style>
--- src/components/plan.vue
+++ src/components/plan.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="body">
-    <div>
-      td 
-      <span>80kg
-      </span>
-    </div>
     <el-row>
-      <el-col :span="6" >d</el-col>
-      <el-col :span="6" >t /2.7</el-col>
-      <el-col :span="6" >d /2.1</el-col>
-      <el-col :span="6" >z </el-col>
+      <el-col :span="6">当前体重</el-col>
+      <el-col :span="6">80kg</el-col>
+    </el-row>
+    <el-row >
+      <el-col :span="6" >摄入标准</el-col>
+      <el-col :span="6" >碳水 /2.7</el-col>
+      <el-col :span="6" >蛋白质 /2.1</el-col>
+      <el-col :span="6" >脂肪 /不计</el-col>
     </el-row>
     <el-row>
-      <el-col :span="6" >d</el-col>
-      <el-col :span="6" >t</el-col>
-      <el-col :span="6" >d</el-col>
-      <el-col :span="6" >z edit esential</el-col>
+      <el-col :span="6" >定量</el-col>
+      <el-col :span="6" >145g</el-col>
+      <el-col :span="6" >100g</el-col>
+      <el-col :span="6" >不计</el-col>
     </el-row>
     <el-row>
-      <el-col :span="6">morning</el-col> 
+      <el-col :span="6">早餐</el-col> 
       <el-col :span="6">45g</el-col>
       <el-col :span="6">45g</el-col>
     </el-row>
     <el-row>
-      <el-col :span="6">launch</el-col>
+      <el-col :span="6">午餐</el-col>
       <el-col :span="6">45g</el-col>
       <el-col :span="6">45g</el-col>
     </el-row>
     <el-row>
-      <el-col :span="6">eing</el-col>
+      <el-col :span="6">晚餐</el-col>
       <el-col :span="6">45g</el-col>
       <el-col :span="6">45g</el-col>
     </el-row>
@@ -46,7 +45,6 @@ export default {
 
 <style scoped lang="scss">
 .body {
-  background-color: #f0f0f0;
   -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); 
   box-shadow: 0px 10px 13px -7px #a1a1a1, 5px 5px 15px 5px rgba(0,0,0,0);
   border-radius: 10px;
@@ -58,30 +56,13 @@ export default {
   border: solid 1px red;
 }
 
-.el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+.el-col{
+  border: solid 1px #ebeef5;
+  min-height: 25px;
+  line-height: 25px;
+}
+
+
+
 </style>
+
